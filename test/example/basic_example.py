79ec320d855eb3006a2e7f68b02ef13861498408
Erik Brozek <erik@brozek.name>
2020-02-05 17:52:02 +0100

    Added: print

--- test/example/basic_example.py
+++ test/example/basic_example.py
@@ -7,15 +7,23 @@ Example for Server API
 
 import sys
 import pprint
-#sys.path.append('/usr/local/lib/python3/dist-packages')
+sys.path.append('/usr/local/lib/python3/dist-packages')
 sys.path.append('server4_api')
 
 import server4_api
 
 API = server4_api.Server4Api(config_file='test/example/serverapi.cfg')
+
+print()
+print('select:')
 pprint.pprint(API.db_select(table_name='test_table', where_dict={'id':1}, limit=5))
+
+print()
+print('execute:')
 pprint.pprint(API.db_execute_dml('SELECT * FROM test_table LIMIT 5'))
 pprint.pprint(API.db_execute_dql('SELECT * FROM test_table LIMIT 5'))
 
+print()
+print('dial:')
 pprint.pprint(API.db_dial_id('test_table', 'id', 'value_str'))
 pprint.pprint(API.db_dial_value('test_table', 'id', 'value_str'))
